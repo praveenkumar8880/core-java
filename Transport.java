77e54619fc2da0c2d0f72c39588a855bc84bf7d3
praveenkumar8880 <npraveenxworkz@gmail.com>
2022-11-23 20:46:00 +0530

    committing java files

---
+++ Transport.java
@@ -0,0 +1,14 @@
+class Transport{
+	//variables and datatypes
+public static void main(String a[]){
+	String name="metro";
+System.out.println("Difference means of transport are"+name);
+
+
+
+}
+
+
+
+
+}
